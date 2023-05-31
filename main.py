4b94f7c48410fd52576f1be27a7e4dcb4167b143
Vinícius M. Teixeira <126170717+ViniTex231@users.noreply.github.com>
2023-05-31 16:54:09 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from inicio import Ui_Inicio
+from test_sync import test
+
+
+class Janela(Ui_Inicio):
+    def __init__(self, MainWindow):
+        super().setupUi(MainWindow)
+        self.bt_iniciar.clicked.connect(self.btn_click)
+
+    def btn_click(self):
+        test()
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    #ui = Ui_MainWindow()
+    #ui.setupUi(MainWindow)
+    janela = Janela(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
